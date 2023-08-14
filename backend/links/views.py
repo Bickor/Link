63d46269b1e242278003828673de840be8087af3
Bickor <martin.heberling.dalies@gmail.com>
2023-08-13 22:53:27 -0700

    Added update page and added option to put company and notes.

--- backend/links/views.py
+++ backend/links/views.py
@@ -7,12 +7,14 @@ from .models import Person
 def index(request):
     persons = Person.objects.all()
     return render(request, "base.html", {"person_list": persons})
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 @require_http_methods(["POST"])
 def add (request):
     name = request.POST["name"]
-    person = Person(name=name, company="NA", notes="NA")
+    company = request.POST["company"]
+    notes = request.POST["notes"]
+    
+    person = Person(name=name, company=company, notes=notes)
     person.save()
     return redirect("index")
 
